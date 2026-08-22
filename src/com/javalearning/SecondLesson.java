90b13fc7d5bf9d8ace9f2a0954534b40b348e408
Danyl Tomchuk <bikokzip@gmail.com>
2026-08-22 14:44:47 +0200

    Second lesson.

--- src/com/javalearning/SecondLesson.java
+++ src/com/javalearning/SecondLesson.java
@@ -1,7 +1,25 @@
 package com.javalearning;
 
 public class SecondLesson {
-    public static void secondLesson(String[] args) {
+    public static void main(String[] args) {
+        byte num1 = 127;
+        short num2 = 32_767;
+        int num3 = 2_147_483_647;
+        long num4 = 9_223_372_036_854_775_807L;
 
+        float num5 = 4.348902f;
+        double num6 = 3.990284d;
+
+        char letter = 'G';
+        String userName = "Steve";
+
+        boolean isHappy = true;
+
+        System.out.println("Age: " + num1);
+
+        num1 = 45;
+        System.out.println(num1);
+
+        System.out.println(userName);
     }
 }
